016eac38410a6ef7b722ff98e18de534269724b2
parckchansame <ased1233@naver.com>
2022-03-29 17:10:09 +0900

    wetube_clone#7_end

--- src/Router/uesrRouter.js
+++ src/Router/uesrRouter.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { Remove, Edituser, see, logout } from "../Controllers/uesrController";
+import {
+  Remove,
+  Edituser,
+  see,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../Controllers/uesrController";
 const userRouter = express.Router();
 
 userRouter.get("/edit", Edituser);
 userRouter.get("/remove", Remove);
-userRouter.get("/:id", see);
 userRouter.get("/logout", logout);
+userRouter.get("/:id", see);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 export default userRouter;
